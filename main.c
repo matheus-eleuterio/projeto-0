db15d0d03615a9eede9c2c2a92ff5574db636d74
Matheus Eleutério <100217154+matheus-eleuterio@users.noreply.github.com>
2024-03-19 22:56:51 +0000

    criacao do menu principal

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello World\n");
-  return 0;
+  int opcao;
+  do {
+
+    printf("\nMenu principal\n");
+    printf("1 - Criar tarefa\n");
+    printf("2 - Deletar tarefa\n");
+    printf("3 - Listar tarefa\n");
+    printf("0 - Sair\n");
+    printf("Escolha uma opção: ");
+    scanf("%d", &opcao);
+    printf("Opção escolhida: %d\n", opcao);
+  } while (opcao != 0);
 }
