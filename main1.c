d9e38bd18b6ee308ffb3f8aec88b2acf11621085
Matheus Eleutério <100217154+matheus-eleuterio@users.noreply.github.com>
2024-03-22 19:24:27 -0300

    Create main1.c

---
+++ main1.c
@@ -0,0 +1,32 @@
+#include "tarefas.h"
+#include <stdio.h>
+
+int main(){
+  int pos = 0;
+  Tarefa tarefas[TOTAL];
+}
+
+int opcao;
+do {
+
+  printf("\nMenu principal\n");
+  printf("1 - Criar tarefa\n");
+  printf("2 - Deletar tarefa\n");
+  printf("3 - Listar tarefa\n");
+  printf("0 - Sair\n");
+  printf("Escolha uma opção: ");
+  scanf("%d", &opcao);
+  printf("Opção escolhida: %d\n", opcao);
+    if(opcao == 1){
+      int erro = criar(tarefas, &pos);
+    } else if(opcao == 2){
+      int erro = deletar(tarefas, &pos);
+    } else if(opcao == 3){
+     int erro = listar(tarefas, pos);
+    {else if(opcao == 0){
+      printf("Sair\n");   
+    } else {
+      printf("Opção inválida\n");
+    }
+    
+} while (opcao != 0);
